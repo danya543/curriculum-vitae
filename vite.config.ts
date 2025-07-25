f6eaba985a3fe9aed70762a584fc37031d04fd45
Daniil <dgilev75@gmail.com>
2025-07-25 13:15:40 +0300

    fix: deploy errors

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  build: {
+  /* build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
@@ -23,5 +23,5 @@ export default defineConfig({
         },
       },
     },
-  },
+  }, */
 })
